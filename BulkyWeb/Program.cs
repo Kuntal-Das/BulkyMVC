5161caf128de8aa1924dfa4ee725cfea60c69b55
Kuntal Das <42152227+Kuntal-Das@users.noreply.github.com>
2024-06-19 09:11:17 +0530

    migration CREATE_TABLE-Categories

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,7 +1,13 @@
+using BulkyWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(opt =>
+opt.UseSqlServer(builder.Configuration.GetConnectionString("MSSQL")));
+
 
 var app = builder.Build();
 
