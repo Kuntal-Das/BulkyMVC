03ba733c598b11a787d216847eb66bd6cfe5f2a1
Kuntal Das <42152227+Kuntal-Das@users.noreply.github.com>
2024-06-28 10:46:11 +0530

    Edit page

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -37,5 +37,35 @@ namespace BulkyWeb.Controllers
             }
             return View();
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (!id.HasValue || id is null || id < 0)
+            {
+                return NotFound();
+            }
+            var category = _db.Categories.Find(id.Value);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            //if (!string.IsNullOrEmpty(category.Name) && category.Name.Contains("test", StringComparison.OrdinalIgnoreCase))
+            //{
+            //    ModelState.AddModelError("Name", "Test is an Invalid value");
+            //}
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
     }
 }
